cca834fd9d140cd8e3bb8e05bdbbd12a8a41d45c
ramhaidar <49301219+ramhaidar@users.noreply.github.com>
2024-12-18 10:31:56 +0700

    refactor: modernize Mininet network topology implementation
    This commit improves the implementation of a mesh network topology using Mininet
    by making the following enhancements:
    
    - Restructure the network topology setup using LinuxRouter class
    - Implement proper IP addressing scheme with 192.168.0.0/16 network
    - Add comprehensive connectivity testing functionality
    - Implement traffic simulation using iperf
    - Add proper routing tables and policy-based routing configuration
    - Improve code organization and readability
    - Add detailed network interface configuration
    - Implement proper cleanup on network termination
    
    The changes provide a more maintainable and robust implementation while
    maintaining the original mesh topology design with 4 routers and 2 hosts.
    Built-in tests ensure proper connectivity and bandwidth allocation across
    the network.

--- Mininet.py
+++ Mininet.py
@@ -1,181 +1,171 @@
-#!/usr/bin/python
-'''
- Source code tugas besar jaringan komputer dibuat oleh:
- Nama : Filza Rahma Muflihah
- NIM  : 1301201261
- Kelas: IF-44-03
-'''
+#!/usr/bin/env python
 
-#import library
-from mininet.log import setLogLevel, info
-from mininet.node import UserSwitch, OVSKernelSwitch
-from mininet.cli import CLI
-from mininet.topo import Topo
 from mininet.net import Mininet
-from mininet.link import TCLink, Link, Intf
-from mininet.node import CPULimitedHost
-from mininet.util import pmonitor
-from signal import SIGINT
-from time import time
-import os
-from subprocess import *
- 
-def topology():
-    #konfigurasi mininet
-    Link = TCLink
-    host = CPULimitedHost
-    net = Mininet(link=Link, host=host)
-   
-    #menambah host
-    h1 = net.addHost('h1')
-    h2 = net.addHost('h2')
-   
-    #menambah router
-    r1 = net.addHost('r1')
-    r2 = net.addHost('r2')
-    r3 = net.addHost('r3')
-    r4 = net.addHost('r4')
-   
-    #setting bandwidth
-    bw1 = {'bw':1} #bandwidth 1 mbps
-    bw2 = {'bw' :0.5} #bandwidth 500 kbps
-   
-    #hubungkan host dengan router sesuai topologi
-    net.addLink(h1,r1, IntfName1 = 'h1-eth0', IntfName2 = 'r1-eth0', cls = TCLink, **bw1)
-    net.addLink(h1,r2, IntfName1 = 'h1-eth1', IntfName2 = 'r2-eth0', cls = TCLink, **bw1)
-    net.addLink(h2,r3, IntfName1 = 'h2-eth0', IntfName2 = 'r3-eth0', cls = TCLink, **bw1)
-    net.addLink(h2,r4, IntfName1 = 'h2-eth1', IntfName2 = 'r4-eth0', cls = TCLink, **bw1)
-    net.addLink(r1,r3, IntfName1 = 'r1-eth1', IntfName2 = 'r3-eth1', cls = TCLink, **bw2)
-    net.addLink(r2,r4, IntfName1 = 'r2-eth1', IntfName2 = 'r4-eth1', cls = TCLink, **bw2)
-    net.addLink(r1,r4, IntfName1 = 'r1-eth2', IntfName2 = 'r4-eth2', cls = TCLink, **bw1)
-    net.addLink(r2,r3, IntfName1 = 'r2-eth2', IntfName2 = 'r3-eth2', cls = TCLink, **bw1)
+from mininet.node import Node
+from mininet.link import TCLink
+from mininet.log import setLogLevel, info
+from mininet.cli import CLI
+import time
 
-    '''
-    #add link + modifikasi buffer size 100
-    net.addLink(h1,r1, max_queue_size = 100, IntfName1 = 'h1-eth0', IntfName2 = 'r1-eth0', cls = TCLink, **bw1)
-    net.addLink(h1,r2, max_queue_size = 100, IntfName1 = 'h1-eth1', IntfName2 = 'r2-eth0', cls = TCLink, **bw1)
-    net.addLink(h2,r3, max_queue_size = 100, IntfName1 = 'h2-eth0', IntfName2 = 'r3-eth0', cls = TCLink, **bw1)
-    net.addLink(h2,r4, max_queue_size = 100, IntfName1 = 'h2-eth1', IntfName2 = 'r4-eth0', cls = TCLink, **bw1)
-    net.addLink(r1,r3, max_queue_size = 100, IntfName1 = 'r1-eth1', IntfName2 = 'r3-eth1', cls = TCLink, **bw2)
-    net.addLink(r2,r4, max_queue_size = 100, IntfName1 = 'r2-eth1', IntfName2 = 'r4-eth1', cls = TCLink, **bw2)
-    net.addLink(r1,r4, max_queue_size = 100, IntfName1 = 'r1-eth2', IntfName2 = 'r4-eth2', cls = TCLink, **bw1)
-    net.addLink(r2,r3, max_queue_size = 100, IntfName1 = 'r2-eth2', IntfName2 = 'r3-eth3', cls = TCLink, **bw1)
-    '''  
-   
-    #build topologi
-    net.build()
- 
-    #konfigurasi router dan host
-    h1.cmd('ifconfig h1-eth0 0')
-    h1.cmd('ifconfig h1-eth1 0')
- 
-    h2.cmd('ifconfig h2-eth0 0')
-    h2.cmd('ifconfig h2-eth1 0')
- 
-    r1.cmd('ifconfig r1-eth0 0')
-    r1.cmd('ifconfig r1-eth1 0')
-    r1.cmd('ifconfig r1-eth2 0')
- 
-    r2.cmd('ifconfig r2-eth0 0')
-    r2.cmd('ifconfig r2-eth1 0')
-    r2.cmd('ifconfig r2-eth2 0')
- 
-    r3.cmd('ifconfig r3-eth0 0')
-    r3.cmd('ifconfig r3-eth1 0')
-    r3.cmd('ifconfig r3-eth2 0')
- 
-    r4.cmd('ifconfig r4-eth0 0')
-    r4.cmd('ifconfig r4-eth1 0')
-    r4.cmd('ifconfig r4-eth2 0')
- 
-    #subnetting
-    h1.cmd('ifconfig h1-eth0 195.30.0.1 netmask 255.255.255.0') #h1-r1
-    h1.cmd('ifconfig h1-eth1 195.30.5.2 netmask 255.255.255.0') #h1-r2
-   
-    r1.cmd('ifconfig r1-eth0 195.30.0.2 netmask 255.255.255.0') #r1-h1
-    r1.cmd('ifconfig r1-eth1 195.30.1.1 netmask 255.255.255.0') #r1-r3
-    r1.cmd('ifconfig r1-eth2 195.30.6.1 netmask 255.255.255.0') #r1-r4
-   
-    r2.cmd('ifconfig r2-eth0 195.30.5.1 netmask 255.255.255.0') #r2-h1
-    r2.cmd('ifconfig r2-eth1 195.30.4.2 netmask 255.255.255.0') #r2-r4
-    r2.cmd('ifconfig r2-eth2 195.30.7.1 netmask 255.255.255.0') #r2-r3
-   
-    h2.cmd('ifconfig h2-eth0 195.30.2.2 netmask 255.255.255.0') #h2-r3
-    h2.cmd('ifconfig h2-eth1 195.30.3.1 netmask 255.255.255.0') #h2-r4
-   
-    r3.cmd('ifconfig r3-eth0 195.30.2.1 netmask 255.255.255.0') #r3-h2
-    r3.cmd('ifconfig r3-eth1 195.30.1.2 netmask 255.255.255.0') #r3-r1
-    r3.cmd('ifconfig r3-eth2 195.30.7.2 netmask 255.255.255.0') #r3-r2
-   
-    r4.cmd('ifconfig r4-eth0 195.30.3.2 netmask 255.255.255.0') #r4-h2
-    r4.cmd('ifconfig r4-eth1 195.30.4.1 netmask 255.255.255.0') #r4-r2
-    r4.cmd('ifconfig r4-eth2 195.30.6.2 netmask 255.255.255.0') #r4-r1
-    
-    #forward semua router
-    r1.cmd("echo 1 > /proc/sys/net/ipv4/ip_forward")
-    r2.cmd("echo 2 > /proc/sys/net/ipv4/ip_forward")
-    r3.cmd("echo 3 > /proc/sys/net/ipv4/ip_forward")
-    r4.cmd("echo 4 > /proc/sys/net/ipv4/ip_forward")
-   
-    #static routing host
-    h1.cmd("ip rule add from 195.30.0.1 table 1")
-    h1.cmd("ip rule add from 195.30.5.2 table 2")
-    h1.cmd("ip route add 195.30.0.0/24 dev h1-eth0 scope link table 1")
-    h1.cmd("ip route add default via 195.30.0.2 dev h1-eth0 table 1")
-    h1.cmd("ip route add 195.30.5.0/24 dev h1-eth1 scope link table 2")
-    h1.cmd("ip route add default via 195.30.5.1 dev h1-eth1 table 2")
-    h1.cmd("ip route add default scope global nexthop via 195.30.0.2 dev h1-eth0")
-   
-    h2.cmd("ip rule add from 195.30.2.2 table 3")
-    h2.cmd("ip rule add from 195.30.3.1 table 4")
-    h2.cmd("ip route add 195.30.2.0/24 dev h2-eth0 scope link table 3")
-    h2.cmd("ip route add default via 195.30.2.1 dev h2-eth0 table 3")
-    h2.cmd("ip route add 195.30.3.0/24 dev h2-eth1 scope link table 4")
-    h2.cmd("ip route add default via 195.30.3.2 dev h2-eth1 table 4")
-    h2.cmd("ip route add default scope global nexthop via 195.30.2.1 dev h2-eth0")
-   
-    #static routing router
-    r1.cmd("route add -net 195.30.2.0/24 gw 195.30.1.2")
-    r1.cmd("route add -net 195.30.3.0/24 gw 195.30.6.2")
-    r1.cmd("route add -net 195.30.4.0/24 gw 195.30.6.2")
-    r1.cmd("route add -net 195.30.5.0/24 gw 195.30.1.2")
-    r1.cmd("route add -net 195.30.7.0/24 gw 195.30.1.2")
-   
-    r2.cmd("route add -net 195.30.0.0/24 gw 195.30.4.1")
-    r2.cmd("route add -net 195.30.1.0/24 gw 195.30.7.2")
-    r2.cmd("route add -net 195.30.2.0/24 gw 195.30.7.2")
-    r2.cmd("route add -net 195.30.3.0/24 gw 195.30.4.1")
-    r2.cmd("route add -net 195.30.6.0/24 gw 195.30.4.1") 
-    
-    r3.cmd("route add -net 195.30.0.0/24 gw 195.30.1.1")
-    r3.cmd("route add -net 195.30.3.0/24 gw 195.30.7.1")
-    r3.cmd("route add -net 195.30.4.0/24 gw 195.30.1.1")
-    r3.cmd("route add -net 195.30.5.0/24 gw 195.30.7.1")
-    r3.cmd("route add -net 195.30.6.0/24 gw 195.30.1.1")   
- 
-    r4.cmd("route add -net 195.30.0.0/24 gw 195.30.6.1")
-    r4.cmd("route add -net 195.30.1.0/24 gw 195.30.6.1")
-    r4.cmd("route add -net 195.30.2.0/24 gw 195.30.4.2")
-    r4.cmd("route add -net 195.30.5.0/24 gw 195.30.4.2")
-    r4.cmd("route add -net 195.30.7.0/24 gw 195.30.4.2")
+class LinuxRouter(Node):
+    """Create a router node with IP forwarding enabled."""
+    def config(self, **params):
+        super(LinuxRouter, self).config(**params)
+        self.cmd('sysctl net.ipv4.ip_forward=1')
 
-    '''
-    #menjalankan background traffic menggunakan iperf
-    h2.cmd('iperf -s &')
-    h1.cmd('iperf -t 60 -c 195.30.2.2')
-    '''
+    def terminate(self):
+        self.cmd('sysctl net.ipv4.ip_forward=0')
+        super(LinuxRouter, self).terminate()
 
-    #menampilkan informasi ping
-    info('\n',net.ping(),'\n')
+def run():
+    net = Mininet(topo=None, build=False, link=TCLink)
+
+    # Add routers
+    r1 = net.addHost('r1', cls=LinuxRouter, ip='192.168.1.1/24', mac='00:00:00:00:01:01')
+    r2 = net.addHost('r2', cls=LinuxRouter, ip='192.168.2.2/24', mac='00:00:00:00:02:02')
+    r3 = net.addHost('r3', cls=LinuxRouter, ip='192.168.3.2/24', mac='00:00:00:00:03:03')
+    r4 = net.addHost('r4', cls=LinuxRouter, ip='192.168.4.2/24', mac='00:00:00:00:04:04')
+
+    # Add hosts
+    h1 = net.addHost('h1', ip='192.168.1.2/24', mac='00:00:00:00:01:02')
+    h2 = net.addHost('h2', ip='192.168.5.2/24', mac='00:00:00:00:05:02')
+
+    # Add links with specified bandwidth
+    # Link with 1 Mbps bandwidth
+    net.addLink(h1, r1, cls=TCLink, bw=1,
+                intfName1='h1-eth0', intfName2='r1-eth0',
+                params1={'ip': '192.168.1.2/24'}, params2={'ip': '192.168.1.1/24'})
     
-    #menghentikan topologi
+    net.addLink(r1, r2, cls=TCLink, bw=1,
+                intfName1='r1-eth1', intfName2='r2-eth0',
+                params1={'ip': '192.168.2.1/24'}, params2={'ip': '192.168.2.2/24'})
+    
+    # Link with 0.5 Mbps bandwidth
+    net.addLink(r2, r3, cls=TCLink, bw=0.5,
+                intfName1='r2-eth1', intfName2='r3-eth0',
+                params1={'ip': '192.168.3.1/24'}, params2={'ip': '192.168.3.2/24'})
+    
+    net.addLink(r3, r4, cls=TCLink, bw=0.5,
+                intfName1='r3-eth1', intfName2='r4-eth0',
+                params1={'ip': '192.168.4.1/24'}, params2={'ip': '192.168.4.2/24'})
+    
+    net.addLink(r4, h2, cls=TCLink, bw=1,
+                intfName1='r4-eth1', intfName2='h2-eth0',
+                params1={'ip': '192.168.5.1/24'}, params2={'ip': '192.168.5.2/24'})
+
+    # Add additional links for mesh topology
+    net.addLink(r1, r3, cls=TCLink, bw=1,
+                intfName1='r1-eth2', intfName2='r3-eth2',
+                params1={'ip': '192.168.6.1/24'}, params2={'ip': '192.168.6.2/24'})
+    
+    net.addLink(r2, r4, cls=TCLink, bw=1,
+                intfName1='r2-eth2', intfName2='r4-eth2',
+                params1={'ip': '192.168.7.1/24'}, params2={'ip': '192.168.7.2/24'})
+
+    net.start()
+
+    # Configure routing for routers
+    r1.cmd('ip route add 192.168.3.0/24 via 192.168.2.2')
+    r1.cmd('ip route add 192.168.4.0/24 via 192.168.2.2')
+    r1.cmd('ip route add 192.168.5.0/24 via 192.168.2.2')
+    r1.cmd('ip route add 192.168.6.0/24 via 192.168.6.2')
+    r1.cmd('ip route add 192.168.7.0/24 via 192.168.2.2')
+
+    r2.cmd('ip route add 192.168.1.0/24 via 192.168.2.1')
+    r2.cmd('ip route add 192.168.4.0/24 via 192.168.3.2')
+    r2.cmd('ip route add 192.168.5.0/24 via 192.168.3.2')
+    r2.cmd('ip route add 192.168.6.0/24 via 192.168.3.2')
+    r2.cmd('ip route add 192.168.7.0/24 via 192.168.7.2')
+
+    r3.cmd('ip route add 192.168.1.0/24 via 192.168.3.1')
+    r3.cmd('ip route add 192.168.2.0/24 via 192.168.3.1')
+    r3.cmd('ip route add 192.168.5.0/24 via 192.168.4.2')
+    r3.cmd('ip route add 192.168.6.0/24 via 192.168.6.1')
+    r3.cmd('ip route add 192.168.7.0/24 via 192.168.4.2')
+
+    r4.cmd('ip route add 192.168.1.0/24 via 192.168.4.1')
+    r4.cmd('ip route add 192.168.2.0/24 via 192.168.4.1')
+    r4.cmd('ip route add 192.168.3.0/24 via 192.168.4.1')
+    r4.cmd('ip route add 192.168.6.0/24 via 192.168.3.2')
+    r4.cmd('ip route add 192.168.7.0/24 via 192.168.7.1')
+
+    # Add routes for the 192.168.6.0/24 network
+    r2.cmd('ip route add 192.168.6.0/24 via 192.168.7.2')
+    r4.cmd('ip route add 192.168.6.0/24 via 192.168.7.1')
+
+    # Configure default gateway for hosts
+    h1.cmd('ip route add default via 192.168.1.1')
+    h2.cmd('ip route add default via 192.168.5.1')
+
+    # Implement policy-based routing for hosts
+    h1.cmd('ip rule add from 192.168.1.2 table 1')
+    h1.cmd('ip route add 192.168.1.0/24 dev h1-eth0 scope link table 1')
+    h1.cmd('ip route add default via 192.168.1.1 table 1')
+
+    h2.cmd('ip rule add from 192.168.5.2 table 2')
+    h2.cmd('ip route add 192.168.5.0/24 dev h2-eth0 scope link table 2')
+    h2.cmd('ip route add default via 192.168.5.1 table 2')
+
+    # Function to test connectivity
+    def test_connectivity():
+        info('*** Testing connectivity:\n')
+        hosts = [h1, h2]
+        routers = [r1, r2, r3, r4]
+        
+        info(f'\n')
+        net.pingAll()
+        info(f'\n')
+
+        # Test connectivity from hosts to routers
+        for host in hosts:
+            for router in routers:
+                for intf in router.intfs.values():
+                    if intf.IP():
+                        result = host.cmd(f'ping -c 1 -W 1 {intf.IP()}')
+                        if '1 received' in result:
+                            info(f'{host.name} -> {router.name} ({intf.IP()}): OK\n')
+                        else:
+                            info(f'{host.name} -> {router.name} ({intf.IP()}): FAILED\n')
+                info(f'\n')
+
+        # Test connectivity from routers to hosts
+        for router in routers:
+            for host in hosts:
+                for intf in host.intfs.values():
+                    if intf.IP():
+                        result = router.cmd(f'ping -c 1 -W 1 {intf.IP()}')
+                        if '1 received' in result:
+                            info(f'{router.name} -> {host.name} ({intf.IP()}): OK\n')
+                        else:
+                            info(f'{router.name} -> {host.name} ({intf.IP()}): FAILED\n')
+            info(f'\n')
+        
+    def test_traffic():
+        info('*** Testing bandwidth between hosts:\n')
+        # Start iperf server on h2
+        h2.cmd('iperf -s &')
+        # Wait for server to start
+        time.sleep(1)
+        # Run iperf client on h1
+        result = h1.cmd('iperf -c ' + h2.IP() + ' -t 10')
+        info(result)
+        # Kill iperf server
+        h2.cmd('killall -9 iperf')
+
+    # Run connectivity tests
+    info('*** Running connectivity test\n')
+    time.sleep(2)  # Wait for routing tables to update
+    test_connectivity()
+    
+    info('*** Running traffic simulation\n')
+    test_traffic()
+
+    info('*** Running CLI\n')
     CLI(net)
-    net.stop()
     
-#main program
-if __name__ == '__main__':
-    os.system('mn -c')
-    os.system('clear')
-    setLogLevel('info')
-    topology()
+    info('*** Stopping network\n')
+    net.stop()
 
+if __name__ == '__main__':
+    setLogLevel('info')
+    run()
